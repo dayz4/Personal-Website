7bb61ec5dc89c4f542e68848a037e3fba7ac369e
Daisy <dhz9@cornell.edu>
2017-08-27 22:12:35 -0400

    update 8/2017

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -15,16 +15,19 @@ $.fn.scrollViewHome = function () {
 }
 
 $(document).ready(function() {
-	$('.header, .circular').hide();
-	$('.bg#home').hide();
+	$('.header').hide();
+	$('.circular').css({'width':'0px', 'height':'0px', 'border-radius':'0px', 'margin-left':'0px', 'top':'600px'})
+	$('.bg#home img').hide();
 	$('.content#about').hide().css('margin-right', '0px');
   $('.nav').load('navbar.html')
 })
 
 $(window).on('load', function() {
-	$('h1#loading').fadeOut(800);
-	$('.header, .circular').css('left', '55%').delay(400).animate({opacity: 'show', left: '50%'}, 1000);
-	$('.bg#home').delay(400).fadeIn(1000);
+	$('h1#loading').fadeOut(400);
+	$('.header').css('left', '55%').delay(400).animate({opacity: 'show', left: '50%'}, 800);
+	$('.circular').delay(1000).animate({width: '600px', height: '600px', 'border-radius': '300px', 'margin-left': '-300px', 'top': '300px'}, 1000);
+	$('.bg#home img').delay(1800).fadeIn(1000);
+	$('.header').delay(1800).animate({'color': 'white'}, 1000);
 });
 
 $(window).scroll(function() {
