1c2efbca65ddb3db02e83ec84d1c4adaac467966
Daisy <dhz9@cornell.edu>
2017-09-12 15:52:07 -0400

    responsiveness with foundation

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+$(document).foundation()
+
 $.fn.scrollView = function () {
     return this.each(function () {
         $('html, body').animate({
@@ -16,16 +18,16 @@ $.fn.scrollViewHome = function () {
 
 $(document).ready(function() {
 	$('.header').hide();
-	$('.circular').css({'width':'0px', 'height':'0px', 'border-radius':'0px', 'margin-left':'0px', 'top':'600px'})
+	$('.circular').css({'width':'0px', 'height':'0px', 'border-radius':'0px', 'margin-left':'0px', 'margin-top':'62vh'})
 	$('.bg#home img').hide();
-	$('.content#about').hide().css('margin-right', '0px');
+	$('.content#about').hide().css('margin-right', '10px');
   	$('.nav').load('navbar.html')
 })
 
 $(window).on('load', function() {
 	$('h1#loading').fadeOut(400);
 	$('.header').css('left', '55%').delay(400).animate({opacity: 'show', left: '50%'}, 800);
-	$('.circular').delay(1000).animate({width: '600px', height: '600px', 'border-radius': '300px', 'margin-left': '-300px', 'top': '300px'}, 1000);
+	$('.circular').delay(1000).animate({width: '60vh', height: '60vh', 'border-radius': '30vh', 'margin-left': '-30vh', 'margin-top': '32vh'}, 1000);
 	$('.bg#home img').delay(1800).fadeIn(1000);
 	$('.header').delay(1200).animate({'color': 'white'}, 1000);
 });
@@ -35,6 +37,6 @@ $(window).scroll(function() {
 	// var aboutTop = $('.content#about').offset().top;
 
 	if (scrollTop > 350) {
-		$('.content#about').animate({opacity: 'show', marginRight: '40px'}, 1000);;
+		$('.content#about').animate({opacity: 'show', marginRight: '50px'}, 1000);;
 	}
 });
