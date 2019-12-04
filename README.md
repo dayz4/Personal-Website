d99947049c34fda0388c997a56a70904f37562f4
Daisy Zheng <skyzheng9@gmail.com>
2019-12-04 02:59:26 -0500

    spring cleaning pt 2

--- README.md
+++
@@ -1,5 +0,0 @@
-# dayz4.github.io
-
-Personal website showcasing my work and providing information about me.
-
-Link: http://daisyzheng.me
---
+++ js/base.js
@@ -0,0 +1,4 @@
+$(document).foundation().ready(function() {
+  	$('.nav').load('navbar.html')
+  	$('.sticky').foundation('_calc', true);
+})
